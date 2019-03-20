471505ba4ada0d0e973f8246e57e2c74326c8a22
BE379 <43654906+BE379@users.noreply.github.com>
2019-03-19 17:28:20 -0700

    adds Assignment10

--- PythonClub/Club/tests.py
+++ PythonClub/Club/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import Meeting, MeetingMinutes, Resource, Event
+from django.urls import reverse
 
 # Create your tests here.
 class MeetingTest(TestCase):
@@ -33,3 +34,83 @@ class EventTest(TestCase):
 
     def test_tablename(self):
         self.assertEqual(str(Event._meta.db_table), 'event')
+
+class TestIndex(TestCase):
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'Club/index.html')
+
+class TestLogin(TestCase):
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('loginmessage'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('loginmessage'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'Club/loginmessage.html')
+
+class TestLogout(TestCase):
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('logoutmessage'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('logoutmessage'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'Club/logoutmessage.html')
+
+class TestMeetingsView(TestCase):
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('getmeetings'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('getmeetings'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'Club/meetings.html')
+
+class TestMinutesView(TestCase):
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('meetingminutes'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('meetingminutes'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'Club/minutes.html')
+
+class TestNewMeeting(TestCase):
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('newMeeting'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('newMeeting'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'Club/newmeeting.html')
+
+class TestNewResource(TestCase):
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('newResource'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('newResource'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'Club/newresource.html')
+
+class TestResourceView(TestCase):
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('resources'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('resources'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'Club/resources.html')
